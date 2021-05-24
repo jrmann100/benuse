e93fb4b9afe82cee37ab0ed8b7a0c98de99b631b
Jordan Mann <jordaloo@gmail.com>
2021-05-23 19:35:02 -0700

    Initial Commit

---
+++ Shared/BenuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  BenuseApp.swift
+//  Shared
+//
+//  Created by Jordan Mann on 5/23/21.
+//
+
+import SwiftUI
+
+@main
+struct BenuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
