fa62549d8d674538e02d4f2dc8e336306b76466e
Jordan Mann <jordaloo@gmail.com>
2021-07-11 01:26:31 -0700

    Refreshed multi-item views.

--- Benuse/Benuse.swift
+++ Benuse/Benuse.swift
@@ -1,15 +1,8 @@
 import SwiftUI
 import WidgetKit
 
-// https://www.themobileentity.com/home/how-to-fetch-image-from-url-with-swiftui-the-easy-way
-extension Image {
-    func data(url: URL) -> Self {
-        if let data = try? Data(contentsOf: url) {
-            return Image(uiImage: UIImage(data: data)!)
-                .resizable()
-        }
-        return resizable()
-    }
+func deepLink(story: HNStory, showItem: Bool) -> URL {
+    return URL(string: "benuse://\(story.id)/\(showItem ? "item" : "comments")\(story.url != nil ? "?url=" + story.url!.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)! : "")")!
 }
 
 struct Provider: TimelineProvider {
@@ -59,55 +52,103 @@ struct SimpleEntry: TimelineEntry {
     let items: [HNStory]
 }
 
-struct SmallItemView: View {
-    let story: HNStory
-    let colorScheme: ColorScheme
-    var body: some View { ZStack {
-        Color(UIColor.systemGray5).edgesIgnoringSafeArea(.all)
-        if story.image != nil {
-            Image(systemName: "newspaper").data(url: story.image!).scaledToFill().frame(
-                minWidth: 0,
-                maxWidth: .infinity,
-                minHeight: 0,
-                maxHeight: .infinity,
-                alignment: .center).clipped().opacity(0.4)
-        }
-        VStack(alignment: .leading) {
-            HStack {
-                Image(systemName: "arrow.up")
-                Text(String(story.score))
-            }.opacity(0.8)
-            Spacer()
-            Text(story.title)
-            Spacer()
-            Text(story.site).font(.system(size: 12, weight: .semibold))
-        }.padding()
+protocol ItemView: View {
+    var story: HNStory { get }
+    var colorScheme: ColorScheme { get }
+}
+
+extension ItemView {
+    private func genImage(_ url: URL?) -> UIImage? {
+        guard let url = url else { return nil }
+        guard let data = (try? Data(contentsOf: url)) else { return nil }
+        return UIImage(data: data)
     }
-    .widgetURL(URL(string: "https://hacker-news.firebaseio.com/v0/item/\(story.id)")!)
+
+    var _uiImage: UIImage? {
+        return genImage(story.image)
+    }
+
+    var image: Image? {
+        guard let uiImage = _uiImage else { return nil }
+        return Image(uiImage: uiImage).resizable()
     }
 }
 
-struct ItemView: View {
+struct SmallItemView: ItemView {
     let story: HNStory
-    let colorScheme: ColorScheme
-    func deepLink(showItem: Bool) -> URL {
-        return URL(string: "benuse://\(story.id)/\(showItem ? "item" : "comments")\(story.url != nil ? "?url=" + story.url!.absoluteString.addingPercentEncoding(withAllowedCharacters: .urlHostAllowed)! : "")")!
+    @Environment(\.colorScheme) var colorScheme
+
+//    private var _uiAverageColor: UIColor? {
+//        guard _uiImage != nil else { return nil }
+//        // https://www.hackingwithswift.com/example-code/media/how-to-read-the-average-color-of-a-uiimage-using-ciareaaverage
+//        guard let inputImage = CIImage(image: _uiImage!) else { return nil }
+//                let extentVector = CIVector(x: inputImage.extent.origin.x, y: inputImage.extent.origin.y, z: inputImage.extent.size.width, w: inputImage.extent.size.height)
+//
+//                guard let filter = CIFilter(name: "CIAreaAverage", parameters: [kCIInputImageKey: inputImage, kCIInputExtentKey: extentVector]) else { return nil }
+//                guard let outputImage = filter.outputImage else { return nil }
+//
+//                var bitmap = [UInt8](repeating: 0, count: 4)
+//        let context = CIContext(options: [.workingColorSpace: kCFNull!])
+//                context.render(outputImage, toBitmap: &bitmap, rowBytes: 4, bounds: CGRect(x: 0, y: 0, width: 1, height: 1), format: .RGBA8, colorSpace: nil)
+//
+//                return UIColor(red: CGFloat(bitmap[0]) / 255, green: CGFloat(bitmap[1]) / 255, blue: CGFloat(bitmap[2]) / 255, alpha: CGFloat(bitmap[3]) / 255)
+//    }
+//
+//    var averageColor: Color {
+//        guard _uiAverageColor != nil else { return .black }
+//        return Color(_uiAverageColor!)
+//    }
+//
+//    var foregroundAverageColor: Color {
+//        guard _uiAverageColor != nil else {return .white}
+//        var hue: CGFloat = 0
+//        var saturation: CGFloat = 0
+//        var brightness: CGFloat = 0
+//        _uiAverageColor?.getHue(&hue, saturation: &saturation, brightness: &brightness, alpha: nil)
+//        return Color(UIColor(hue: 0, saturation: 0, brightness: brightness <= 0.5 ? 1 : 0, alpha: 1))
+//    }
+    
+    var averageColor = Color(UIColor.systemGray)
+    var foregroundAverageColor = Color.white
+
+    var body: some View { GeometryReader { metrics in
+        ZStack {
+            (self.averageColor).edgesIgnoringSafeArea(.all)
+            VStack(spacing: 0) {
+                ZStack(alignment: Alignment(horizontal: .center, vertical: .bottom)) {
+                    (self.image ?? Image(systemName: "newspaper"))
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: metrics.size.width, height: metrics.size.height * 0.6)
+                        .clipped()
+                            LinearGradient(gradient: Gradient(colors: [.clear, self.averageColor]), startPoint: .top, endPoint: .bottom).frame(height: 80)
+                            Text("↑ \(story.score)  \(story.site)")
+
+                                .font(.system(size: 11))
+                                .lineLimit(1)
+                                .padding(.horizontal, 10)
+                }
+                Text(story.title)
+                    .font(.system(size: 14, weight: .semibold))
+                    .padding(.horizontal, 10)
+                    .frame(maxHeight: .infinity)
+            }
+            .widgetURL(deepLink(story: story, showItem: true))
+            .foregroundColor(self.foregroundAverageColor)
+        }
     }
+    }
+}
+
+struct BigItemView: ItemView {
+    let story: HNStory
+    @Environment(\.colorScheme) var colorScheme
 
     var body: some View {
-        ZStack {
-            if story.image != nil {
-                Image(systemName: "newspaper").data(url: story.image!).scaledToFill().frame(
-                    minWidth: 0,
-                    maxWidth: .infinity,
-                    minHeight: 0,
-                    maxHeight: .infinity,
-                    alignment: .center).clipped().opacity(0.5)
-            }
-            HStack {
-                Link(destination: deepLink(showItem: story.url != nil), label: {
+        HStack (alignment: .center) {
+                Link(destination: deepLink(story: story, showItem: story.url != nil)) {
                     VStack(alignment: .leading) {
-                        Text(story.site).font(.system(size: 13, weight: .semibold))
+                        Text(story.site).font(.system(size: 11, weight: .semibold)).foregroundColor(.secondary)
                         Spacer().frame(maxHeight: 5)
                         HStack {
                             Group {
@@ -119,12 +160,16 @@ struct ItemView: View {
                     }.onTapGesture(perform: {
                         // TODO: gray out visited links (could shuffle, but might want comments)
                     })
-                    Link(destination: deepLink(showItem: false)) {
-                        Image(systemName: "text.bubble.fill")
-                            .font(.system(size: 30))
+                }
+            if self.image != nil {
+                    Link(destination: deepLink(story: story, showItem: false)) { // should this still link?
+                        (self.image!)
+                            .scaledToFill()
+                            .frame(width: 50, height: 50, alignment: .center)
+                            .clipped()
+                            .cornerRadius(8)
                     }
-                })
-            }.frame(maxHeight: 35).padding()
+            }
         }
     }
 }
@@ -140,20 +185,20 @@ struct ItemsView: View {
         // TODO: translucency is not currently possible
         //       (it worked in Today widgets, so we'll see)
         if widgetFamily == .systemSmall {
-            SmallItemView(story: entry.items[0], colorScheme: colorScheme)
+            SmallItemView(story: entry.items[0])
         } else {
+            // TODO: different categories? top, ask, show?
             ZStack {
-                // TODO: header with title
-                Color(UIColor.systemGray5).edgesIgnoringSafeArea(.all)
-                VStack(alignment: .leading) {
-                    if widgetFamily == .systemLarge {
-                        Link(destination: URL(string: "benuse://home")!) { Text("Hacker News").foregroundColor(.orange).font(.system(size: 17, weight: .heavy)).padding(EdgeInsets(top: 15, leading: 15, bottom: 5, trailing: 10)) }
-                    }
-                    ForEach(entry.items, id: \.self) { item in
-                        ItemView(story: item, colorScheme: colorScheme)
-                    }
+                Color("WidgetColor").edgesIgnoringSafeArea(.all)
+                VStack(alignment: .leading, spacing: 5) {
+//                if widgetFamily == .systemLarge {
+                    Link(destination: URL(string: "benuse://home")!) { Text("Hacker News").foregroundColor(.orange).font(.system(size: 17, weight: .heavy)) }
+//                }
+                ForEach(entry.items, id: \.self) { item in
+                    BigItemView(story: item)
                 }
-            }
+            }.padding(EdgeInsets(top: 10, leading: 15, bottom: 0, trailing: 15))
+        }
         }
     }
 }
@@ -166,5 +211,13 @@ struct Benuse: Widget {
         }
         .configurationDisplayName("Hacker News")
         .description("Displays today's best articles.")
+    }
+}
+
+struct Benuse_Previews: PreviewProvider {
+    static let dummy = HNStory(_item: HNAPIStory(id: 0, type: HNAPIType.story, by: "jrmann100", time: 0, text: nil, kids: [], url: "https://jrmann.com", score: 299, title: "Benuse - Bringing Hacker News to the iOS home screen", descendants: 0), site: "Jordan's Projects", image: URL(string: "https://images.saymedia-content.com/.image/t_share/MTc2Mjg0OTI2Mzc3ODYyMzM0/reading-newspaper-as-a-habit.jpg")!)
+    static var previews: some View {
+                SmallItemView(story: dummy).previewContext(WidgetPreviewContext(family: .systemSmall))
+
     }
 }
